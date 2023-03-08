953e4f8d5a4156021aa95b53919552efad9d7f6b
GeorginaCarvajal <georginacarvajal@colegioguanenta.edu.co>
2023-03-08 07:03:38 -0500

    se creo el programa

--- ejercicio_2/ejercicio_1.py
+++ ejercicio_2/ejercicio_1.py
@@ -1,1 +1,21 @@
-print("Hola")
+#Programa que permita realizar un préstamo bancario
+
+print("------------------------------------------------")
+print("---Programa para otorgar un prestamo bancario---")
+print("------------------------------------------------")
+
+#input
+ing = int(input("Digite los ing: "))
+deuda = int(input("Digite la deuda: "))
+
+# processing
+if ing > 945200:
+    if deuda == 0:
+        msj = ("Prestamo Aprobado")
+    else:
+        msj = ("Prestamo No Aprobado")
+else:
+        msj = ("Prestamo No Aprobado")
+
+# output
+print (msj)
